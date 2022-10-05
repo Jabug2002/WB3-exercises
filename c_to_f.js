d58a2812980b8eaa34c0cf8dd0cf36cccbdfd19e
Jabug2002 <jadenodavis@gmail.com>
2022-10-05 13:08:00 -0400

    New File

---
+++ c_to_f.js
@@ -0,0 +1,9 @@
+function convertCtoF(c) {
+    return (c+32)*1.8
+} 
+console.log(convertCtoF(100))
+console.log(convertCtoF(45))
+console.log(convertCtoF(19))
+console.log(convertCtoF(0))
+console.log(convertCtoF(-7))
+console.log(convertCtoF(-40))
