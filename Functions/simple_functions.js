67741056345f88a7c20e85d116c01dd656ecfac7
Jabug2002 <jadenodavis@gmail.com>
2022-10-05 08:36:53 -0400

    Files made

---
+++ Functions/simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings() {
+    console.log ("Jaden Davis")
+    console.log("Iron Man")
+    console.log("Chris Brown")
+}
+
+function whyImhere() {
+    console.log("For financial freedom.")
+}
+
+function favoritePlace() { 
+    console.log("Orlando, Florida")
+}
+
+favoriteThings()
+whyImhere()
+favoritePlace()
