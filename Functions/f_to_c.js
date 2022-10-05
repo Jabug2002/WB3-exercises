9e24dd61a617f4dec07c2421e8aea2c8bac62f08
Jabug2002 <jadenodavis@gmail.com>
2022-10-05 09:39:55 -0400

    New file

---
+++ Functions/f_to_c.js
@@ -0,0 +1,12 @@
+function convertFtoC(f) {
+    return (f-32)*.5556
+}
+// let result=convertFtoC(100)
+// console.log(result)
+console.log(convertFtoC(100))
+console.log(convertFtoC(212))
+console.log(convertFtoC(90))
+console.log(convertFtoC(72))
+console.log(convertFtoC(32))
+console.log(convertFtoC(0))
+console.log(convertFtoC(-40))
